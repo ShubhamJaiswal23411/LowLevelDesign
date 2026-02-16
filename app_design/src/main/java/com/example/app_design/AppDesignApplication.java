3bb4ac3f4360367c2122910dd1c25ee79d9f4cdf
Shubham Moto <motobook@gmail.com>
2026-02-16 20:14:07 +0530

    Added app design project

--- app_design/src/main/java/com/example/app_design/AppDesignApplication.java
+++ app_design/src/main/java/com/example/app_design/AppDesignApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.app_design;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AppDesignApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AppDesignApplication.class, args);
 	}
 
 }
--- app_design/src/test/java/com/example/app_design/AppDesignApplicationTests.java
+++ app_design/src/test/java/com/example/app_design/AppDesignApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.app_design;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AppDesignApplicationTests {
 
 	@Test
 	void contextLoads() {
